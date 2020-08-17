b42b63eccdbd32e7bb81fc1b114224e8c1cdd86a
SongKyeongYong <ruuddyd@gmail.com>
2020-08-17 19:14:51 +0900

    1463 DP

---
+++ DP/1463.py
@@ -0,0 +1,24 @@
+from _collections import deque
+N = int(input())
+
+def bfs(s):
+    queue = deque()
+    queue.append([s,0])
+    visited = {s:True}
+    while queue:
+        n, c = queue.popleft()
+        if n == 1:
+            return c
+        else:
+            c += 1
+            if n-1 not in visited:
+                queue.append([n-1,c])
+                visited[n-1] = True
+            if n%3==0 and n/3 not in visited:
+                queue.append([n/3,c])
+                visited[n/3] = True
+            elif n%2==0 and n/2 not in visited:
+                queue.append([n/2,c])
+                visited[n/2] = True
+
+print(bfs(N))
