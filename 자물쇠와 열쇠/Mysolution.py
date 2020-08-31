7e5a737391002d4e89f8710be7247ccd56e95073
SongKyeongYong <ruuddyd@gmail.com>
2020-08-31 19:15:49 +0900

    자물쇠와 열쇠, 2차원 배열 다루기

--- 자물쇠와 열쇠/Mysolution.py
+++ 자물쇠와 열쇠/Mysolution.py
@@ -1,94 +1,47 @@
-import copy
-from _collections import deque
+def rotation(m):
+    N = len(m)
+    ret = [[0] * N for _ in range(N)]
 
-visited = []
-
-
-def rotation(k):
-    after = [[0] * 3 for _ in range(3)]
-    for i in range(3):
-        for j in range(3):
-            after[i][j] = k[2 - j][i]
-    return after
+    for r in range(N):
+        for c in range(N):
+            ret[c][N-1-r] = m[r][c]
+    return ret
 
 
-def inRange(tx, ty):
-    if -1 <= tx <= 3 and -1 <= ty <= 3:
-        if not (tx == -1 and ty == -1) and not (tx == -1 and ty == 3) \
-                and not (tx == 3 and ty == -1) and not (tx == 3 and ty == 3):
-            return True
-    return False
-
-
-def bfs(key, lock):
-    N = len(key)
-    M = len(lock)
-
-    queue = deque()
-    visited.append([1, 1])  # visited
-    queue.append([1, 1, key])  # init center
-
-    while queue:
-        succeed = True
-        tx, ty, k = queue.popleft()
-        for a in range(3):
-            for b in range(3):
-                if k[a][b] + lock[a][b] != 1:
-                    succeed = False
-        if succeed:
-            return True
-
-        # push to left
-        ax = tx
-        ay = ty - 1
-        if inRange(ax, ay) and [ax, ay] not in visited:
-            visited.append([ax, ay])
-            n_matrix = [[0] * 3 for _ in range(3)]
-            for i in range(3):
-                for j in range(2):
-                    n_matrix[i][j] = k[i][j + 1]
-            queue.append([ax, ay, n_matrix])
-
-        # push to right
-        ax = tx
-        ay = ty + 1
-        if inRange(ax, ay) and [ax, ay] not in visited:
-            visited.append([ax, ay])
-            n_matrix = [[0] * 3 for _ in range(3)]
-            for i in range(3):
-                for j in range(1, 3):
-                    n_matrix[i][j] = k[i][j - 1]
-            queue.append([ax, ay, n_matrix])
-
-        # push to up
-        ax = tx - 1
-        ay = ty
-        if inRange(ax, ay) and [ax, ay] not in visited:
-            visited.append([ax, ay])
-            n_matrix = [[0] * 3 for _ in range(3)]
-            for i in range(2):
-                for j in range(3):
-                    n_matrix[i][j] = k[i + 1][j]
-            queue.append([ax, ay, n_matrix])
-
-        # push to down
-        ax = tx + 1
-        ay = ty
-        if inRange(ax, ay) and [ax, ay] not in visited:
-            visited.append([ax, ay])
-            n_matrix = [[0] * 3 for _ in range(3)]
-            for i in range(1, 3):
-                for j in range(3):
-                    n_matrix[i][j] = k[i - 1][j]
-            queue.append([ax, ay, n_matrix])
-
-        # rotation
-        r_matrix = k
-        for l in range(3):
-            r_matrix = rotation(r_matrix)
-            queue.append([tx, ty, r_matrix])
-    return False
-
+def expand(N, lock):
+    expansion = [[0] * 3 * N for _ in range(3 * N)]
+    for i in range(N, 2 * N):
+        for j in range(N, 2 * N):
+            expansion[i][j] = lock[i - N][j - N]
+    return expansion
 
 def solution(key, lock):
-    return bfs(key, lock)
+    answer = True
+
+    M = len(key)
+    N = len(lock)
+
+    for _ in range(4):
+        key = rotation(key) # rotation
+        for i in range(3*N):
+            for j in range(3*N):
+                succeed = True
+                expansion = expand(N, lock) # expansion
+
+                for a in range(M):
+                    for b in range(M):
+                        if N<=a+i<2*N and N<=b+j<2*N: # in range, cal
+                            expansion[a+i][b+j] += key[a][b]
+
+                for x in range(N, 2*N):
+                    for y in range(N, 2*N):
+                        if expansion[x][y] != 1: # checking
+                            succeed = False
+                            break
+                    if not succeed:
+                        break
+                if succeed:
+                    return True
+    return False
+
+print(solution([[0, 0, 0], [1, 0, 0], [0, 1, 1]], [[1, 1, 1], [1, 1, 0], [1, 0, 1]]))
