d757721c1388755a6a1c6601c179aae477ecf9e1
SongKyeongYong <ruuddyd@gmail.com>
2020-09-30 11:12:56 +0900

    init

--- test.py
+++ test.py
@@ -1,17 +1,39 @@
-N = int(input())
-count = 0
-for i in range(1, N+1):
-    if i < 10:
-        count += 1
-        continue
 
-    l = len(str(i)) - 1
-    k = 0
-    d = int(str(i)[k]) - int(str(i)[k+1])
-    while k < l:
-        if d != int(str(i)[k]) - int(str(i)[k+1]):
-            break
-        k += 1
-    if k == l: # pass
-        count += 1
-print(count)
+
+def solution(blocks):
+    answer = []
+    N = len(blocks)
+    matrix = [[0]* N for _ in range(N)]
+    h = 0
+    for i in blocks:
+        matrix[h][i[0]] = i[1]
+        h += 1
+
+    H = 1
+
+    while H <= N - 1:
+        for n in range(H+1): # 각 행의 개수만큼
+            if matrix[H][n] != 0:
+                idx = n
+                break
+
+
+        l = idx
+        while 0 <= l - 1: # left
+            matrix[H][l-1] = matrix[H-1][l-1] - matrix[H][l]
+            l -= 1
+
+        r = idx
+        while r + 1 <= H: # right
+            matrix[H][r + 1] = matrix[H-1][r] - matrix[H][r]
+            r += 1
+        H += 1
+
+    for i in range(N):
+        for j in range(i+1):
+            answer.append(matrix[i][j])
+
+    return answer
+
+
+print(solution([[0, 50], [0, 22], [2, 10], [1, 4], [4, -13]]	))
