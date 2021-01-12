bd2fef4ce28a8b940368eaaac966518af2827ebb
Dragon <ruuddyd@gmail.com>
2021-01-12 16:38:11 +0900

    visited[] 효율적 처리

--- DFS, BFS/2667.py
+++ DFS, BFS/2667.py
@@ -1,47 +1,37 @@
 from _collections import deque
 
-N = int(input())
-matrix = []
-visit = []
-length = []
-for _ in range(N):
-    matrix.append(list(map(int, list(input()))))
-for _ in range(N):
-    li = []
-    for __ in range(N):
-        li.append(0)
-    visit.append(li)
-opx = [-1,1,0,0]
-opy = [0,0,-1,1]
-
-def bfs(x, y):
-    global c
+def bfs(x, y, c):
     queue = deque()
-    queue.append([x,y])
-    visit[x][y] = 1
+    queue.append([x, y])
     while queue:
         ax, ay = queue.popleft()
-        #print(ax,ay)
-        c+=1
+        c += 1
         for k in range(4):
             xx = ax + opx[k]
             yy = ay + opy[k]
-            if 0<=xx<=N-1 and 0<=yy<=N-1:
-                if visit[xx][yy]==0 and matrix[xx][yy] == 1: # not visited
-                    visit[xx][yy] = 1
-                    queue.append([xx,yy])
+            if 0 <= xx <= N-1 and 0 <= yy <= N-1:
+                if matrix[xx][yy] == 1: # not use visited[]..
+                    matrix[xx][yy] = 0
+                    queue.append([xx, yy])
+    return c
 
 
+N = int(input())
+matrix, length = [], []
+
+for _ in range(N):
+    matrix.append(list(map(int, list(input()))))
+
+opx = [-1,1,0,0]
+opy = [0,0,-1,1]
 
 for i in range(N):
     for j in range(N):
-        if not visit[i][j] and matrix[i][j]:
-            c = 0
-            #print(f'i j = {i},{j}')
-            bfs(i,j)
-            length.append(c)
+        if matrix[i][j]:
+            matrix[i][j] = 0
+            length.append(bfs(i, j, 0))
 
 print(len(length))
 length.sort()
-for i in length:
-    print(i)
+for l in length:
+    print(l)
