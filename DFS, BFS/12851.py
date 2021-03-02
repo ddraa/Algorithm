41774ff47fd525702856b4f1028eea79b5089055
Dragon <ruuddyd@gmail.com>
2021-03-02 22:41:18 +0900

    BFS 다중 경로 가짓수 체크

--- DFS, BFS/12851.py
+++ DFS, BFS/12851.py
@@ -1,12 +1,10 @@
 import sys
 from collections import deque
-sys.setrecursionlimit(10 ** 6)
 
 def bfs():
-    global ans
     queue = deque()
     queue.append((n, 0))
-    m = 0
+    m, ans = 0, 0
 
     while queue:
         cur, c = queue.popleft()
@@ -14,24 +12,16 @@ def bfs():
             m = c
             ans += 1
 
-        if cur + 1 <= 10 ** 5 and (not visited[cur + 1] or c + 1 == visited[cur + 1]):
-            visited[cur + 1] = c + 1
-            queue.append((cur + 1, c + 1))
-
-        if 0 <= cur - 1 and (not visited[cur - 1] or c + 1 == visited[cur - 1]):
-            visited[cur - 1] = c + 1
-            queue.append((cur - 1, c + 1))
-
-        if cur * 2 <= 10 ** 5 and (not visited[cur * 2] or c + 1 == visited[cur * 2]):
-            visited[cur * 2] = c + 1
-            queue.append((cur * 2, c + 1))
-    return m
+        for Next in [cur + 1, cur - 1, cur * 2]:
+            if 0 <= Next <= 10 ** 5 and (not visited[Next] or c + 1 == visited[Next]):
+                visited[Next] = c + 1
+                queue.append((Next, c + 1))
+    return m, ans
 
 
 n, k = map(int, sys.stdin.readline().split())
-visited = [0 for _ in range(10 ** 5 + 1)]
+visited = [0 for _ in range(10 ** 5 + 1)] # 최단거리 확인 + 방문 체크
 visited[n] += 1
-ans = 0
-MIN = bfs()
-print(MIN)
-print(ans)
+a, b = bfs()
+print(a)
+print(b)
